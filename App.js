eb1c84347bf0ffc2d8bebe5e7154e71c598bfd7c
AltaciosTheDev <133562096+AltaciosTheDev@users.noreply.github.com>
2024-01-26 15:27:31 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,102 @@
+import React from "react"
+import Die from "./Die.js"
+import { nanoid } from 'nanoid'
+import Confetti from 'react-confetti'
+
+export default function App(){
+    //default value for state will be allNewDice
+    const [dice, setDice] = React.useState(allNewDice())
+    const [tenzies, setTenzies] = React.useState(false)
+    
+    //function to generate random number that will be called into allNewDice
+    function generateRandomDie(){
+        let random = Math.ceil(Math.random() * 6)
+        let die = {
+            value: random,
+            isHeld: false,
+            id: nanoid()
+        }
+        return die
+    }
+    
+    //Perfect use of a useEffect: when we need to constantly check on a state which will determine the state of another state.
+    React.useEffect(() => {
+        const allHeld = dice.every(die => die.isHeld)
+        const refValue = dice[0].value
+        const allValue = dice.every(die => die.value === refValue)
+        if(allHeld && allValue){
+            setTenzies(true)
+        }
+        //apparently, can't short circuit and use an && in the same statement. 
+    }, [dice])
+    
+    //Function that will run 10 times to give us newDiceNumbers
+    function allNewDice(){
+        let newDiceArray = []
+        for(let i=0;i<10;i++){
+            newDiceArray.push(generateRandomDie())
+        }
+        return newDiceArray
+    }
+    
+    //Function that will roll the dice again, BUT will not roll dice that are hold.
+    function rollDice(){
+        //ternary operator conditional 
+        !tenzies ? 
+            setDice(oldDice => {
+                 return oldDice.map(oldDie => {
+                     return oldDie.isHeld ? oldDie: generateRandomDie()
+                 })
+             })
+            :
+            setDice(allNewDice())
+            setTenzies(false)
+    
+        //traditional if else conditionals 
+        // if(!tenzies){
+        //     setDice(oldDice => {
+        //         return oldDice.map(oldDie => {
+        //             return oldDie.isHeld ? oldDie: generateRandomDie()
+        //         })
+        //     })  
+        // }
+        // else{
+        //     setDice(allNewDice())
+        //     setTenzies(false)
+        // }
+    }
+    
+    //function that will will allow each specific die to identify and modify itself 
+    function holdDice(id){
+        setDice(oldDice => {
+            return oldDice.map(oldDie => {
+                return oldDie.id === id ? {...oldDie, isHeld: !oldDie.isHeld} : oldDie
+            })
+        })
+    }
+    
+    //Map over dice to render Die component for each number in the array 
+    const diceElements = dice.map(die => 
+        <Die 
+            // {...die} sending the entire destructured array as props.property 
+            //{die}     sending the entire arrray (props.die.property)
+            value={die.value}
+            isHeld={die.isHeld}
+            holdDice={() => holdDice(die.id)}
+            key={die.id}
+        />)
+
+    return(
+        <main>
+            {tenzies && <Confetti />}
+            <h1 className="title">Tenzies</h1>
+            <p className="instruction">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+            <div className="dice-container">
+                {diceElements}
+            </div>
+            <button className="roll-dice" onClick={rollDice}>{tenzies?"New Game":"Roll"}</button>
+        </main>
+    )
+}
+
+
---
+++ notes.js
@@ -0,0 +1,19 @@
+//create Die component 
+//create newDice function 
+//create state variable to hold the dice array 
+//create button that will roll the array of dice again 
+//Make the dice an object in the function that generates random numbers 
+
+//Holding dice 
+    //part1: conditionally render the styles object depending on the isHeld property 
+    //part2: each die able to hold when we click and flip its propery(which will change the background)
+    //part3:  whenever we roll, only the isheld: false dice get rolled 
+        //bonus: create entirely separate function for generating new die object 
+        
+//End game 
+    //part 1: useState for tenzies 
+    //part 2: useEffect for changing tenzies to true 
+    //part 3: conditionally render confetti and change button text based on tenzie win 
+//new game
+    //conditional functioning for the roll button depending on the state of tenzies 
+    //flip tenzies 
